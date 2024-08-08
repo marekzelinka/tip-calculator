23b4e63dc5b5fac18b803b7fede49ad2f66c0b23
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 21:48:19 +0200

    reactor disabled resutls

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx'
 import { useFormContext } from 'react-hook-form'
 import { formatCurrency } from '../utils.js'
 
@@ -12,32 +11,41 @@ export function Results() {
   const tipPerPerson = isValid ? (bill * tip) / peopleCount : 0
   const totalPerPerson = isValid ? bill / peopleCount + tipPerPerson : 0
 
+  const stats = [
+    { name: 'Tip', value: tipPerPerson },
+    { name: 'Total', value: totalPerPerson },
+  ]
+
   return (
     <dl className="space-y-6">
-      <div>
-        <dt className="text-sm/6 font-medium text-gray-600">Tip</dt>
-        <dd
-          className={clsx(
-            'mt-2 flex items-baseline gap-x-2 text-4xl font-semibold tracking-tight',
-            tipPerPerson === 0 ? 'text-gray-500' : 'text-emerald-600',
-          )}
-        >
-          {formatCurrency(tipPerPerson)}
-          <span className="text-sm text-gray-500">/ person</span>
-        </dd>
-      </div>
-      <div>
-        <dt className="text-sm/6 font-medium text-gray-600">Total</dt>
-        <dd
-          className={clsx(
-            'mt-2 flex items-baseline gap-x-2 text-4xl font-semibold tracking-tight',
-            totalPerPerson === 0 ? 'text-gray-500' : 'text-emerald-600',
-          )}
-        >
-          {formatCurrency(totalPerPerson)}
-          <span className="text-sm text-gray-500">/ person</span>
-        </dd>
-      </div>
+      {stats.map((stat) => {
+        const disabled = stat.value === 0 ? '' : undefined
+
+        return (
+          <div key={stat.name} data-disabled={disabled}>
+            <dt
+              className="text-sm/6 font-medium text-gray-500 data-[disabled]:opacity-50"
+              data-disabled={disabled}
+            >
+              {stat.name}
+            </dt>
+            <dd className="mt-2 flex items-baseline gap-x-2">
+              <span
+                className="text-3xl/10 font-medium tracking-tight text-gray-900 data-[disabled]:opacity-50"
+                data-disabled={disabled}
+              >
+                {formatCurrency(stat.value)}
+              </span>
+              <span
+                className="text-xs font-medium text-gray-700 data-[disabled]:opacity-50"
+                data-disabled={disabled}
+              >
+                / person
+              </span>
+            </dd>
+          </div>
+        )
+      })}
     </dl>
   )
 }
