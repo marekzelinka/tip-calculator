2e2ac3d0b88e09048699173d598d44aee378b01c
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 21:00:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="isolate flex min-h-svh flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="mt-8 text-balance text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Split your expenses with your{' '}
+          <span className="text-emerald-600">friends & colleagues</span>
+        </h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
