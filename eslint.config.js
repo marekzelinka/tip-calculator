3c14cd447057d67787a909df7f2c5eb1d77d6030
Marek Zelinka <mzelinka17@gmail.com>
2025-05-12 09:46:33 +0200

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 
-export default [
+export default defineConfig([
+  { ignores: ["dist"] },
   {
     files: ["**/*.{js,jsx}"],
-    ignores: ["dist"],
+    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -17,22 +18,17 @@ export default [
         sourceType: "module",
       },
     },
-    settings: { react: { version: "18.3" } },
     plugins: {
-      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
   },
-];
+]);
